671aa459e41a9abb6af95d08e070b46106d26203
Mason Fisher <sackzombie10@gmail.com>
2019-08-04 23:10:32 -0400

    Adding controls

--- src/main.c
+++ src/main.c
@@ -4,10 +4,17 @@
 
 void init();
 void uswitch();
+void getinput();
 
 void main() {
   init();
   while(1) {
+    /*
+    9 = middle X coord
+    19 = max X coord
+    8 = middle Y coord
+    17 = max Y coord
+    */
     uswitch();
     wait_vbl_done();
   }
@@ -16,11 +23,27 @@ void main() {
 void init() {
   DISPLAY_ON;
   set_bkg_data(0, 4, tiles);
-  set_bkg_tiles(2,6,1,1,tilebk);
+  set_bkg_tiles(9,8,1,1,tilelg);
 }
 
 void uswitch() {
   HIDE_WIN;
   SHOW_SPRITES;
   SHOW_BKG;
+}
+
+void getinput() {
+
+  if (joypad() & J_B) {
+  // The B button was pressed!
+  } else if (joypad() & J_UP) {
+    // The UP button was pressed!
+  } else if (joypad() & J_DOWN) {
+    // The DOWN button was pressed!
+  } else if (joypad() & J_LEFT) {
+    // The LEFT button was pressed!
+  } else if (joypad() & J_RIGHT) {
+    // The RIGHT button was pressed!
+  }
+
 }
