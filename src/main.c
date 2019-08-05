72a128ac3abd22406dbb7323bc7570e67e330f7b
Mason Fisher <sackzombie10@gmail.com>
2019-08-04 22:22:10 -0400

    Added more functions

--- src/main.c
+++ src/main.c
@@ -1,5 +1,22 @@
 #include <gb/gb.h>
 
+void init();
+void uswitch();
+
 void main() {
-  
+  init();
+  while(1) {
+    uswitch();
+    wait_vbl_done();
+  }
+}
+
+void init() {
+  DISPLAY_ON;
+}
+
+void uswitch() {
+  HIDE_WIN;
+  SHOW_SPRITES;
+  SHOW_BKG;
 }
